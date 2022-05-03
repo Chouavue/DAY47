b9819f1c499e2b7f01d81a3c7414eae6c2ac1d7a
Choua Vue <vuec23@gmail.com>
2022-05-03 10:06:14 -0400

    first commit

---
+++ src/VariableInputs.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VariableInputs {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string of text:");
+        String string = scanner.nextLine();
+
+        System.out.println("Enter an integer:");
+        int integer = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Enter a double:");
+        double decimal = Double.valueOf(scanner.nextLine());
+
+        System.out.println("Enter a boolean value:");
+        boolean  value= Boolean.valueOf(scanner.nextLine());
+
+        System.out.println("Your string is "+ string);
+        System.out.println("Your integer is " + integer);
+        System.out.println("Your double is " + decimal);
+        System.out.println("Your boolean is " + value);
+        scanner.close ();
+    }
+}
